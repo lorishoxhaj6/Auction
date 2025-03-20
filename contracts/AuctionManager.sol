03fd70be1f53440df40fa929d0e533b5b6ebbcd4
mattiadane <mattia.danese04@gmail.com>
2025-03-20 15:31:29 +0100

    end of project

--- contracts/AuctionManager.sol
+++ contracts/AuctionManager.sol
@@ -1,19 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-import 'contracts/Auction.sol';
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import "contracts/Auction.sol";
 
 contract AuctionManager {
-    
     Auction[] public auctions;
 
-    event AuctionCreated(Auction auctionAdress, address seller);
+    event AuctionCreated(Auction auctionAdress, address owner);
 
     // Crea una nuova asta e la registra nel gestore
     function AddAuction(Auction newAuction) public {
-
         // collega l'asta ad un gestore
         auctions.push(newAuction);
 
@@ -24,8 +20,4 @@ contract AuctionManager {
     function getAuctionCount() external view returns (uint) {
         return auctions.length;
     }
-
-
-    
 }
-
--- test/AuctionManager.ts
+++ test/AuctionManager.ts
@@ -1,34 +1,62 @@
-const { loadFixture } = require("@nomicfoundation/hardhat-networks-helpers");
-const { expect } = require("chai");
-import { ethers } from "hardhat"
+//import  { loadFixture } from "@nomicfoundation/hardhat-networks-helpers";
+
+import { network, ethers } from "hardhat";
+import { expect } from "chai";
+
 
 describe("Auction Manager contract",function(){
+  let auctionManager: any;
+  let auction: any;
+  let owner: any;
 
-  async function deployAuctionManager() {
-    // oggetti che ci permettono di interargire con il contratto
+  beforeEach(async function () {
+    // Ottieni i firmatari
+    [owner] = await ethers.getSigners();
+
+    // Distribuisci AuctionManager
     const AuctionManager = await ethers.getContractFactory("AuctionManager");
+    auctionManager = await AuctionManager.deploy();
+   
+
+    // Distribuisci Auction
     const Auction = await ethers.getContractFactory("Auction");
-
+    auction = await Auction.deploy(300); // Imposta durata asta
     
-    const auctionmanager = await AuctionManager.deploy();
-    const auction = await Auction.deploy();
     
-    const [owner, addr1, addr2] = await ethers.getSigners();
-    
-    await auctionmanager.deployed();
-    await auction.deployed();
-
-    return {owner,addr1,addr2, auction, auctionmanager};
-  }
+  });
 
   it("Should emit AuctionCreated event", async function () {
 
-    const {auctionmanager, owner, auction} = await loadFixture(deployAuctionManager);
-    
-    expect(await auctionmanager.AddAuction(auction.address))
-    .to.emit(auctionmanager,"AuctionCreated")
+    await expect(auctionManager.AddAuction(auction))
+    .to.emit(auctionManager, "AuctionCreated")
     .withArgs(auction,owner.address);
 
-  })
+  });
+
+  it("Should add auction done ", async function () {
+      await auctionManager.AddAuction(auction);
+
+      expect(await auctionManager.getAuctionCount()).to.eq(1);
+
+  });
+
+
+
+  it("Should get auction count", async function () {
+    expect(await auctionManager.getAuctionCount()).to.eq(0);
+
+    await auctionManager.AddAuction(auction);
+
+
+    expect(await auctionManager.getAuctionCount()).to.eq(1);
+
+
+  });
+
+
+
+
+
+
 
 })
